0a4151cd28932f18f4144693cf8e51239e49fb22
Alexander Sabouret <asaboure@student.42.fr>
2022-08-28 12:19:56 +0200

    added ping command

--- functions/handlers/handleCommands.js
+++ functions/handlers/handleCommands.js
@@ -1,16 +1,30 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const fs = require('fs');
 
 module.exports = (client) => {
     client.handleCommands = async () => {
         const commandFolders = fs.readdirSync('./commands');
         for (const folder of commandFolders) {
-            const commandFiles = fs.readdirSync(`.commands/${folder}`).filter((file) => file.endsWith(".js"));
+            const commandFiles = fs.readdirSync(`./commands/${folder}`).filter((file) => file.endsWith(".js"));
             const { commands, commandArray } = client;
             for (const file of commandFiles){
                 const command = require(`../../commands/${folder}/${file}`);
                 commands.set(command.data.name, command);
-                commandArray.push(command, command.data.toJSON())
+                commandArray.push(command.data.toJSON())
             }
+        }
+        const clientId = '688035147559337994';
+        const guildId = '708111231789301891';
+        const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+        try {
+            console.log('Started refreshing apllication (/) commands.');
+            await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+                body: client.commandArray,
+            });
+            console.log('Successfully reloaded application (/) commands.')
+        } catch (error) {
+            console.error(error);
         }
     }
 }
