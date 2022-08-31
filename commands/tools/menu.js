2dfa4a316d4fc7aee3fcaa6d0a1b269129a2f51f
Alexander SABOURET <asaboure@e1r12p9.clusters.42paris.fr>
2022-08-31 15:25:59 +0200

    now using menu command to chose a database match

--- commands/tools/menu.js
+++ commands/tools/menu.js
@@ -1,23 +1,29 @@
 const { SlashCommandBuilder, SelectMenuBuilder, ActionRowBuilder, SelectMenuOptionBuilder } = require('discord.js');
 const testMenu = require('../../components/selectMenus/testMenu');
+const { count } = require('../../schemas/match');
+const Match = require ('../../schemas/match');
+
+
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('menu')
         .setDescription('idk dude'),
     async execute(interaction, client) {
+        options = [];
+        const count = await Match.estimatedDocumentCount();
+        for (i = 0; i < count - 1; i++){
+            matchProfile = await Match.findOne({matchId: i + 1});
+            options[i] = {
+                label: "match ID: " + matchProfile.matchId,
+                value: matchProfile.playerLeft + " vs " + matchProfile.playerRight + " " + i
+            };
+        }
         const menu = new SelectMenuBuilder()
             .setCustomId('testMenu')
             .setMinValues(1)
             .setMaxValues(1)
-            .setOptions(new SelectMenuOptionBuilder({
-                label: `lisablush`,
-                value: 'one'
-            }), new SelectMenuOptionBuilder({
-                label: 'pepekama',
-                value: 'two'
-            })
-            );
+            .addOptions(options);
         await interaction.reply({
             components: [new ActionRowBuilder().addComponents(menu)]
         });
