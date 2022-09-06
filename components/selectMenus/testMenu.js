4872518762a241c059325291cf1a9629e21127e9
Alexander SABOURET <asaboure@e1r9p10.clusters.42paris.fr>
2022-09-06 16:46:40 +0200

    now using testMenu to delete a match entry

--- components/selectMenus/testMenu.js
+++ components/selectMenus/testMenu.js
@@ -1,10 +1,19 @@
+Match = require ('../../schemas/match');
+
 module.exports = {
     data: {
         name: `testMenu`
     },
     async execute (interaction, client) {
         await interaction.reply({
-            content: `you select: ${interaction.values[0]}`,
+            content: `match deleted: ${interaction.values[0]}`,
         });
+        await Match.deleteOne({matchId: interaction.values[0]});
+        for (i = interaction.values[0]; i < client.matchCount; i++){
+            matchProfile = await Match.findOne({matchId: i});
+            matchProfile.matchId--;
+            matchProfile.save();
+        }
+        client.matchCount--;
     },
 };
