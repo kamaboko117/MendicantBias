690707ca3b12eb15cebaa0aa6f184fe273d8a68d
Alexander SABOURET <asaboure@e2r11p4.clusters.42paris.fr>
2022-09-08 19:38:15 +0200

    match buttons now increase vote count

--- components/buttons/default.js
+++ components/buttons/default.js
@@ -1,10 +1,32 @@
+const Match = require ('../../schemas/match');
+
 module.exports = {
     data: {
         name: 'default'
     },
     async execute(interaction, client) {
-        await interaction.reply({
-            content: 'no code for this button'
-        })
+        console.log(interaction.component.customId.split(' ')[0]);
+        const matchId = interaction.component.customId.split(' ');
+        matchProfile = await Match.findOne({matchId: matchId[0]});
+        console.log(matchProfile);
+        if (!matchProfile){
+            await interaction.reply({
+                content: 'no code for this button'
+            })
+        }else{
+            if (matchId[1] == 'left'){
+                matchProfile.votesLeft++;
+                await interaction.reply({
+                    content: 'voted for: ' + matchProfile.playerLeft
+                })
+            }else if (matchId[1] == 'right'){
+                matchProfile.votesRight++;
+                await interaction.reply({
+                    content: 'voted for: ' + matchProfile.playerRight
+                })
+            }
+            await matchProfile.save().catch(console.error);
+            
+        }
     }
 }
