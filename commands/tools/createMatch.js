356947a91be4570db8fc86e7818cbb5982be53eb
Alexander SABOURET <asaboure@e2r11p9.clusters.42paris.fr>
2022-09-09 16:31:32 +0200

    commented embed to prevent cluttering

--- commands/tools/createMatch.js
+++ commands/tools/createMatch.js
@@ -20,28 +20,7 @@ module.exports = {
     async execute(interaction, client) {
         const option1 = interaction.options.getString('emote1');
         const option2 = interaction.options.getString('emote2');
-        const embed = new EmbedBuilder()
-            .setTitle('random match')
-            // .setDescription('MATCH')
-            .setColor(client.color)
-            // .setThumbnail('https://www.pngkey.com/png/full/898-8989988_crossed-swords-crossed-swords-emoji.png')
-            .setFooter({
-                iconURL: 'https://www.dlf.pt/png/big/10/109431_picardia-png.png',
-                text: 'emotedokai season 2'
-            })
-            .setURL('https://challonge.com')
-            .addFields([
-                {
-                    name: `emote 1`,
-                    value: option1,
-                    inline: true
-                },
-                {
-                    name: 'emote 2',
-                    value: option2,
-                    inline: true
-                }
-            ]);
+        
         const emote1 = client.emojis.cache.find(emoji => emoji.name === option1.split(':')[1]);
         const emote2 = client.emojis.cache.find(emoji => emoji.name === option2.split(':')[1]);
 
@@ -63,20 +42,45 @@ module.exports = {
         });
         await matchProfile.save().catch(console.error);
         client.matchCount++;
+
+        //create embed
+        const embed = new EmbedBuilder()
+            .setTitle(`Match ${matchProfile.matchId}`)
+            // .setDescription('MATCH')
+            .setColor(client.color)
+            // .setThumbnail('https://www.pngkey.com/png/full/898-8989988_crossed-swords-crossed-swords-emoji.png')
+            // .setFooter({
+            //     iconURL: 'https://www.dlf.pt/png/big/10/109431_picardia-png.png',
+            //     text: 'emotedokai season 2'
+            // })
+            .setURL('https://challonge.com')
+            // .addFields([
+            //     {
+            //         name: `emote 1`,
+            //         value: option1,
+            //         inline: true
+            //     },
+            //     {
+            //         name: 'emote 2',
+            //         value: option2,
+            //         inline: true
+            //     }
+            // ]);
+
         //create buttons
         const button1 = new ButtonBuilder()
             .setCustomId(matchProfile._id + ' left')
             // .setLabel(option1)
-            .setStyle(ButtonStyle.Primary)
+            .setStyle(ButtonStyle.Secondary)
             .setEmoji(emote1.id);
         const button2 = new ButtonBuilder()
             .setCustomId(matchProfile._id + ' right')
             // .setLabel("2")
-            .setStyle(ButtonStyle.Primary)
+            .setStyle(ButtonStyle.Secondary)
             .setEmoji(emote2.id);
 
         await interaction.reply({
-                embeds: [embed],
+                // embeds: [embed],
                 components: [
                     new ActionRowBuilder().addComponents(button1, button2)
                 ]
