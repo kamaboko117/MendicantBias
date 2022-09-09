794a0cb5f950cbcde175c8378b4c1fc232410ec4
Alexander SABOURET <asaboure@e2r11p9.clusters.42paris.fr>
2022-09-09 15:19:15 +0200

    now using emoji object directly

--- schemas/match.js
+++ schemas/match.js
@@ -1,15 +1,16 @@
 const { Schema, model } = require('mongoose');
+
 const matchSchema = new Schema({
     _id: Schema.Types.ObjectId,
     matchId: Number,
-    playerLeft: String,
-    playerRight: String,
+    playerLeft: Schema.Types.Mixed,
+    playerRight: Schema.Types.Mixed,
     votesLeft: Number,
     votesRight: Number,
     membersLeft: [String],
     membersRight: [String],
     open: Boolean,
-    winner: String
+    winner: Schema.Types.Mixed,
 });
 
 module.exports = model("Match", matchSchema, "matches");
