3357c9cf929a909b5afd7d624cb81fba54647cae
Alexander SABOURET <asaboure@e2r11p9.clusters.42paris.fr>
2022-09-09 17:01:54 +0200

    fixed deletion

--- components/selectMenus/deleteMenu.js
+++ components/selectMenus/deleteMenu.js
@@ -5,15 +5,15 @@ module.exports = {
         name: `deleteMenu`
     },
     async execute (interaction, client) {
-        await interaction.reply({
-            content: `match deleted: ${interaction.values[0]}`,
-        });
         await Match.deleteOne({matchId: interaction.values[0]});
-        for (i = interaction.values[0] + 1; i < client.matchCount + 1; i++){
+        for (i = Number(interaction.values[0]) + 1; i < client.matchCount + 1; i++){
             matchProfile = await Match.findOne({matchId: i});
             matchProfile.matchId--;
             matchProfile.save().catch(console.error);
         }
         client.matchCount--;
+        await interaction.reply({
+            content: `match deleted: ${interaction.values[0]}`,
+        });
     },
 };
