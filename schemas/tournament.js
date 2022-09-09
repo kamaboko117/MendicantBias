ba551c07c2349d57f0bc87c2cf57c49a5d38a7b3
Alexander SABOURET <asaboure@e2r11p9.clusters.42paris.fr>
2022-09-09 18:57:00 +0200

    removed playercount from tourney schema

--- schemas/tournament.js
+++ schemas/tournament.js
@@ -4,7 +4,6 @@ const { Schema, model } = require('mongoose');
 const tournamentSchema = new Schema({
     id: Schema.Types.ObjectId,
     name: String,
-    playerCount: Number,
     players: [String]
 });
 
