4743847e0991223ec2aaebc6e47b73dc71de7cbb
Alexander SABOURET <asaboure@e2r11p9.clusters.42paris.fr>
2022-09-09 18:37:37 +0200

    completed tournament schema

--- schemas/tournament.js
+++ schemas/tournament.js
@@ -1,6 +1,11 @@
-Match = require `./match`;
+Match = require(`./match`);
 const { Schema, model } = require('mongoose');
 
 const tournamentSchema = new Schema({
     id: Schema.Types.ObjectId,
+    name: String,
+    playerCount: Number,
+    players: [String]
 });
+
+module.exports = model("Tournament", tournamentSchema, "tournaments");
