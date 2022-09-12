6a00e3800f04e116a4fb93e4ec524339e3d4de90
Alexander SABOURET <asaboure@e1r8p9.clusters.42paris.fr>
2022-09-12 16:52:48 +0200

    fixed tournament schema

--- schemas/tournament.js
+++ schemas/tournament.js
@@ -4,7 +4,10 @@ const { Schema, model } = require('mongoose');
 const tournamentSchema = new Schema({
     id: Schema.Types.ObjectId,
     name: String,
-    players: [String]
+    players: [String],
+    playerCount: Number,
+    winnerRounds: [Schema.ObjectId],
+    loserRounds: [Schema.ObjectId]
 });
 
 module.exports = model("Tournament", tournamentSchema, "tournaments");
