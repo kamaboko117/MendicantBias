76bf7fd36f9d152e31508c878d28450542425d82
Alexander SABOURET <asaboure@e1r4p10.clusters.42paris.fr>
2022-09-13 15:24:07 +0200

    tourneynext fully implemented

--- commands/tools/tourneyNext.js
+++ commands/tools/tourneyNext.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const Round = require('../../schemas/round');
-const Match = require ('../../schemas/match');
+const { Match } = require ('../../schemas/match');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -8,27 +8,35 @@ module.exports = {
         .setDescription('post next matches for tournament'),
     async execute(interaction, client) {
         roundProfile = await Round.findOne({name: 'Ro512'})
+        await interaction.reply({
+            content: "next matches:"
+        });
         for (i = 0; i < 16; i++)
-        {
-            matchProfile = Match.findOne({_id: roundProfile.matches[i]});
-            const button1 = new ButtonBuilder()
-            .setCustomId(matchProfile._id + ' left')
-            .setStyle(ButtonStyle.Secondary)
-            .setEmoji(matchProfile.playerLeft);
-            
-            const button2 = new ButtonBuilder()
-            .setCustomId(matchProfile._id + ' right')
-            .setStyle(ButtonStyle.Secondary)
-            .setEmoji(emote2.playerRight);
-            await interaction.reply({
-                content: "pouet"
-            });
+        {        
+            matchProfile = await Match.findById(roundProfile.matches[i]);
+            console.log(matchProfile.playerLeft);
+            console.log(matchProfile.playerRight);
+            if (matchProfile.playerRight){
+                
+                emote1 = roundProfile.matches[i].playerLeft.split(':')[2].slice(0, -1);
+                emote2 = roundProfile.matches[i].playerRight.split(':')[2].slice(0, -1);
 
-            await interaction.reply({
-                components: [
+                console.log(emote1);
+                const button1 = new ButtonBuilder()
+                .setCustomId(matchProfile._id + ' left')
+                .setStyle(ButtonStyle.Secondary)
+                .setEmoji(emote1);
+                
+                const button2 = new ButtonBuilder()
+                .setCustomId(matchProfile._id + ' right')
+                .setStyle(ButtonStyle.Secondary)
+                .setEmoji(emote2);
+                interaction.channel.send({components: [
                     new ActionRowBuilder().addComponents(button1, button2)
-                ]
-            });
+                ]})
+            }else{
+                interaction.channel.send(`${i + 1} bye`)
+            }
         }
     }
 }
