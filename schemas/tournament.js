53a9774e20165ab745c9d828a5899abf26262ca9
Alexander SABOURET <asaboure@e1r5p10.clusters.42paris.fr>
2022-09-14 19:56:57 +0200

    added more elements to tournament schema

--- schemas/tournament.js
+++ schemas/tournament.js
@@ -1,14 +1,17 @@
 const { Match } = require(`./match`);
 const { Schema, model } = require('mongoose');
+const { roundSchema } = require('./round');
 
 const tournamentSchema = new Schema({
     id: Schema.Types.ObjectId,
     name: String,
     players: [String],
     playerCount: Number,
-    winnerRounds: [Schema.ObjectId],
-    loserRounds: [Schema.ObjectId],
-    currentRound: String,
+    winnerRounds: [roundSchema],
+    loserRounds: [roundSchema],
+    currentBracket: Number,
+    currentWinner: Number,
+    currentLoser: Number,
     currentMatch: Number,
     open: Boolean 
 });
