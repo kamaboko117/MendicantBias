a2567bbead25e9fc00dd3a08bddb5586004a67fa
Alexander SABOURET <asaboure@e1r11p10.clusters.42paris.fr>
2022-09-16 20:20:58 +0200

    improved reveal command

--- commands/tools/reveal.js
+++ commands/tools/reveal.js
@@ -1,4 +1,12 @@
-const { SlashCommandBuilder, SelectMenuBuilder, ActionRowBuilder, SelectMenuOptionBuilder } = require('discord.js');
+const {
+    SlashCommandBuilder,
+    SelectMenuBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    SelectMenuOptionBuilder
+} = require('discord.js');
+
 const testMenu = require('../../components/selectMenus/revealMenu');
 const { Match } = require ('../../schemas/match');
 
@@ -13,8 +21,16 @@ module.exports = {
             ),
 
     async execute(interaction, client) {
+        console.log(`${interaction.member.displayName} used reveal`)
         options = interaction.options.getString('match_id');
-        if (options){
+        // if (options){
+            if (isNaN(options))
+            {
+                await interaction.reply({
+                    content: `Error: ID should be a number`,
+                });
+                return ;
+            }
             matchProfile = await Match.findOne({matchId: options});
             if (!matchProfile){
                 await interaction.reply({
@@ -24,34 +40,48 @@ module.exports = {
             matchProfile.open = false;
             matchProfile.winner = matchProfile.votesRight > matchProfile.votesLeft ? matchProfile.playerRight : matchProfile.playerLeft;
             await matchProfile.save().catch(console.error); 
-            await interaction.reply({
-                content: `${interaction.member} match results:\n${matchProfile.playerLeft} ${matchProfile.votesLeft} | ${matchProfile.votesRight} ${matchProfile.playerRight}\n${matchProfile.winner} wins !`,
-            });
-            return ;
-        }
-        options = [];
-        const count = client.matchCount;
-        if (!count){
-            await interaction.reply({
-                content: "no matches"
-            });
-            return ;
-        }
-        for (i = 0; i < count; i++){
-            matchProfile = await Match.findOne({matchId: i + 1});
-            options[i] = {
-                label: "match ID: " + matchProfile.matchId,
-                // value: matchProfile.playerLeft + " vs " + matchProfile.playerRight + " " + i
-                value: `` + matchProfile.matchId
-            };
-        }
-        const menu = new SelectMenuBuilder()
-            .setCustomId('revealMenu')
-            .setMinValues(1)
-            .setMaxValues(1)
-            .addOptions(options);
+            // return ;
+        // }
+        // options = [];
+        // const count = client.matchCount;
+        // if (!count){
+        //     await interaction.reply({
+        //         content: "no matches"
+        //     });
+        //     return ;
+        // }
+        // for (i = 0; i < count; i++){
+        //     matchProfile = await Match.findOne({matchId: i + 1});
+        //     options[i] = {
+        //         label: "match ID: " + matchProfile.matchId,
+        //         // value: matchProfile.playerLeft + " vs " + matchProfile.playerRight + " " + i
+        //         value: `` + matchProfile.matchId
+        //     };
+        // }
+        // const menu = new SelectMenuBuilder()
+        //     .setCustomId('revealMenu')
+        //     .setMinValues(1)
+        //     .setMaxValues(1)
+        //     .addOptions(options);
+        
+        const emote1 = matchProfile.playerLeft.split(':')[2].slice(0, -1);
+        const emote2 = matchProfile.playerRight.split(':')[2].slice(0, -1);
+
+        const button1 = new ButtonBuilder()
+            .setCustomId(matchProfile._id + ' left')
+            .setStyle(ButtonStyle.Secondary)
+            .setLabel(`[${matchProfile.votesLeft}]`)
+            .setEmoji(emote1);
+            
+        const button2 = new ButtonBuilder()
+            .setCustomId(matchProfile._id + ' right')
+            .setStyle(ButtonStyle.Secondary)
+            .setLabel(`[${matchProfile.votesRight}]`)
+            .setEmoji(emote2);
+
         await interaction.reply({
-            components: [new ActionRowBuilder().addComponents(menu)]
+            content: `${interaction.member} match results:\n${matchProfile.playerLeft} ${matchProfile.votesLeft} | ${matchProfile.votesRight} ${matchProfile.playerRight}\n${matchProfile.winner} wins !`,
+            components: [new ActionRowBuilder().addComponents(button1, button2)]
         });
     },
 };
