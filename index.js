f6ddd40e46ef653ce3ff8504538d01faa78e8112
Alexander SABOURET <asaboure@e2r9p4.clusters.42paris.fr>
2022-10-04 19:17:12 +0200

    now using secondary database for offensive bias

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 const token = process.env.TOKEN2;
-const databaseToken = process.env.DATABASE;
+const databaseToken = process.env.DATABASE2;
 const { connect } = require('mongoose');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const fs = require('fs');
