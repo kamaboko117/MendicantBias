1868b96efc2295574074d7390a2ad54fd1f874a4
Alexander SABOURET <asaboure@e1r10p18.clusters.42paris.fr>
2022-10-24 15:58:29 +0200

    added support for unicode emojis in createMatch

--- commands/singleMatches/createMatch.js
+++ commands/singleMatches/createMatch.js
@@ -1,6 +1,7 @@
 const { Match } = require ('../../schemas/match');
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const mongoose = require('mongoose');
+const emojiRegex = require('emoji-regex');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -20,10 +21,23 @@ module.exports = {
     async execute(interaction, client) {
         const option1 = interaction.options.getString('emote1');
         const option2 = interaction.options.getString('emote2');
+        const re = emojiRegex();
+        console.log(`option1: ${option1}`)
         
-        const emote1 = client.emojis.cache.find(emoji => emoji.name === option1.split(':')[1]);
-        const emote2 = client.emojis.cache.find(emoji => emoji.name === option2.split(':')[1]);
+        let emote1 = client.emojis.cache.find(emoji => emoji.name === option1.split(':')[1]);
+        let unicode1 = false;
+        let emote2 = client.emojis.cache.find(emoji => emoji.name === option2.split(':')[1]);
+        let unicode2 = false;
 
+        //if no emotes are found in cache, we check if the emotes are unicode
+        if (!emote1){
+            unicode1 = true;
+            emote1 = re.exec(option1)[0];
+        }
+        if (!emote2){
+            unicode2 = true;
+            emote2 = re.exec(option2)[0];
+        }
         if (!emote1 || !emote2){
             await interaction.reply({
                 content: "Emojis missing from database: add me to the source server"
@@ -41,8 +55,8 @@ module.exports = {
         matchProfile = new Match({
             _id: mongoose.Types.ObjectId(),
             matchId: matchId,
-            playerLeft: emote1,
-            playerRight: emote2,
+            playerLeft: `${emote1}`,
+            playerRight: `${emote2}`,
             votesLeft: 0,
             votesRight: 0,
             open: true
@@ -79,12 +93,12 @@ module.exports = {
             .setCustomId(matchProfile._id + ' left')
             // .setLabel(option1)
             .setStyle(ButtonStyle.Secondary)
-            .setEmoji(emote1.id);
+            .setEmoji(unicode1 ? emote1 : emote1.id);
         const button2 = new ButtonBuilder()
             .setCustomId(matchProfile._id + ' right')
             // .setLabel("2")
             .setStyle(ButtonStyle.Secondary)
-            .setEmoji(emote2.id);
+            .setEmoji(unicode2 ? emote2 : emote2.id);
 
         await interaction.reply({
                 // embeds: [embed],
@@ -97,7 +111,6 @@ module.exports = {
 
     usage: 'Use this command to create an emote 1v1. I will reply with your **matchID** \
     and the match. Once you are ready to display \
-    the results, use \`/reveal <matchID>\`.  Only use custom emojis: default\
-    emojis are not supported yet.\n**I cannot use an emoji if I\'m not a member of the \
+    the results, use \`/reveal <matchID>\`.\n**I cannot use an emoji if I\'m not a member of the \
     emoji\'s source server**'
 };
