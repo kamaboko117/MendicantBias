b66abd195543ac3f32a229a0727146eddd707c66
Alexander SABOURET <asaboure@student.42.fr>
2022-11-24 18:11:31 +0100

    reformatted code with prettier

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,49 +1,50 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('queue')
-        .setDescription('get the music queue'),
-    
+        .setName("queue")
+        .setDescription("get the music queue"),
+
     async execute(interaction, client) {
-        console.log(`${interaction.member.displayName} used /queue`)
+        console.log(`${interaction.member.displayName} used /queue`);
         //create embed
-        let fields = []
-        let titles = []
-        let queue = client.queues.find(queue => queue.id === interaction.guild.id)
+        let fields = [];
+        let titles = [];
+        let queue = client.queues.find(
+            (queue) => queue.id === interaction.guild.id
+        );
         if (queue)
-        queue = queue.queue
-        if (!queue || queue.isEmpty){
+            queue = queue.queue;
+        if (!queue || queue.isEmpty) {
             await interaction.reply({
                 content: "Queue is empty",
                 ephemeral: false,
-            })
-            return ;
+            });
+            return;
         }
-        titles[0] = `**▶️ ${queue.elements[queue.head].metadata.title}**`
-        let j = 1
-        for (let i = queue.head + 1; i < queue.tail; i++){
-            titles[j] =  `**${j++}:** ${queue.elements[i].metadata.title}`
+        titles[0] = `**▶️ ${queue.elements[queue.head].metadata.title}**`;
+        let j = 1;
+        for (let i = queue.head + 1; i < queue.tail; i++) {
+            titles[j] = `**${j++}:** ${queue.elements[i].metadata.title}`;
         }
         let i = 0;
-        for (const title of titles){
+        for (const title of titles) {
             fields[i] = new Object();
             fields[i].name = "\u200B";
             fields[i++].value = title;
-            if (i === 25)
-                break;
+            if (i === 25) break;
         }
         const embed = new EmbedBuilder()
-        .setDescription(`track list`)
-        .setColor(client.color)
-        .addFields(fields)
+            .setDescription(`track list`)
+            .setColor(client.color)
+            .addFields(fields);
 
         await interaction.reply({
             // content: "yo",
             ephemeral: false,
             embeds: [embed],
-        })
+        });
     },
 
-    usage: ''
-}
+    usage: "",
+};
