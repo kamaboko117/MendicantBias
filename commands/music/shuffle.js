89538b936dfc0cce13d195d1794abb1ebf3129ab
Alexander SABOURET <asaboure@student.42.fr>
2022-11-28 17:55:52 +0100

    implemented shuffle command to shuffle the queues

---
+++ commands/music/shuffle.js
@@ -0,0 +1,45 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { getVoiceConnection } = require("@discordjs/voice");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("shuffle")
+        .setDescription("shuffles the current playlist"),
+
+    async execute(interaction, client) {
+        console.log(`${interaction.member.displayName} used /shuffle`);
+        const { voice } = interaction.member;
+        if (!voice.channelId) {
+            interaction.reply("Error: You are not in a voice channel");
+            return;
+        }
+        const connection = getVoiceConnection(interaction.guild.id);
+        if (!connection) {
+            await interaction.reply({
+                content: "Nothing to shuffle",
+            });
+            return;
+        }
+
+        let queue = client.queues.find(
+            (queue) => queue.id === interaction.guild.id
+        );
+        if (queue) queue = queue.queue;
+        let tmpArray = [];
+
+        for (let i = queue.head + 1; i < queue.tail; i++){
+            tmpArray.push(queue.elements[i])
+        }
+        tmpArray.sort(() => Math.random() - 0.5);
+        tmpArray.push(queue.elements[queue.head]);
+        while (!queue.isEmpty) queue.dequeue();
+        let size = tmpArray.length;
+        for (let i = 0; i < size; i++)
+            queue.enqueue(tmpArray.pop());
+        await interaction.reply({
+            content: "Done",
+        });
+    },
+
+    usage: "",
+};
