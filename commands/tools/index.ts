ce669bed8b0bd7dc4f53519f3c1b1fc10542a1ab
Alexander Sabouret <asaboure@student.42.fr>
2024-02-13 18:26:23 +0100

    removed useless command

--- commands/tools/index.ts
+++ commands/tools/index.ts
@@ -1,12 +1,10 @@
 import help from "./help";
-import insultejo from "./insultejo";
 import invite from "./invite";
 import ping from "./ping";
 import weather from "./weather";
 
 export default {
   help,
-  insultejo,
   invite,
   ping,
   weather,
